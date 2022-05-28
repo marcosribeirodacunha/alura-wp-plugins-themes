fbee016d6213d8f7e51a84136a4e63731f8406ba
Marcos RC <mrcmarcos100@gmail.com>
2022-05-28 12:06:26 -0300

    feat: initial commit and alura theme

---
+++ wp-content/themes/alura/page.php
@@ -0,0 +1,17 @@
+<?php
+get_header();
+if (have_posts()): the_post(); ?>
+  <section class="titulo-page container">
+    <?php the_title();?>
+  </section>
+
+  <?php
+  the_content();
+endif; ?>
+
+  <section class="sidebar-page">
+      <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("widget-alura")); ?>
+  </section>
+  </section>
+<?php
+get_footer();
